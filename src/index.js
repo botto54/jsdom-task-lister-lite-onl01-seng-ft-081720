41fe99965f5824d810eff4cd42155730e3bc93da
botto54 <bryanlbottorff@gmail.com>
2020-12-03 06:23:03 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const form = () => document.querySelector('form')
+const input = () => document.querySelector('input#new-task-description')
+const tList = () => document.querySelector('ul#tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form().addEventListener('submit', createTask)
+
 });
+
+function createTask(e) {
+  e.preventDefault()
+  
+  const description = input().value
+  
+  const li = document.createElement('li')
+  li.innerText = description
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = "Delete!"
+
+  deleteButton.addEventListener('click', function() {
+    this.parentNode.remove()
+  })
+
+  li.appendChild(deleteButton)
+  tList().appendChild(li)
+
+  
+  clearInput()
+}
+
+function clearInput() {
+  input().value = ''
+}
